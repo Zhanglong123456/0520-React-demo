3658b26967d21ffa3e64b42ca5651f0294ed39dc
zhanglong <75827293@qq.com>
2019-09-11 11:15:32 +0800

    删除了idea文件

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,10 +1,22 @@
 import React,{Component} from "react"
 
+import {From,Input,Icon,Button} from "antd";
+
+import "./index.less"
+
+import logo from "./logo.png"
+
  class Login extends Component{
 
     render() {
-        return <div>
-            Login
+
+        // const {getFieldDecorator}=this.props.from;
+
+        return <div className="login">
+            <header className="hearer-login">
+                <img src={logo} alt="logo"/>
+                <h1>React项目: 后台管理系统</h1>
+            </header>
         </div>
     }
 }
